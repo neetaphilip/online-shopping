b04c8097b2bd84b29fa4e142853d64b8635965eb
Neeta Philip <neeta.philip@gmail.com>
2019-11-06 12:43:37 -0500

    Project Setup completed.

---
+++ shopping-backend-app/src/main/java/nep/projects/shopping_backend_app/App.java
@@ -0,0 +1,13 @@
+package nep.projects.shopping_backend_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
